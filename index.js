131e72a3f97807c3f68d0b3fa8a953e89d9af4b6
Ross Forster <rforster@sfu.ca>
2022-01-27 11:51:01 -0800

    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('public/calculator'))
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/db', async (req, res) => {
